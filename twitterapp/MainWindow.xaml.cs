07f373905c8393fffed8a41aae92dae276cd4bc0
Justin Strayer <justincstrayer@gmail.com>
2011-10-27 06:41:49 -0500

    v 0.1

---
+++ twitterapp/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace twitterapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+          
+        }
+
+        public void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            //This array is going to hold the data from the text file
+            String[] Stringdata;
+            Stringdata = new String[50];
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                //try
+                //{
+                    // Create an instance of StreamReader to read from a file.
+                    // The using statement also closes the StreamReader.
+                    using (StreamReader sr = new StreamReader(dlg.FileName))
+                    {
+                        String line;
+                        // Read and display lines from the file until the end of
+                        // the file is reached.
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            int counter = 0;
+                            Console.WriteLine(line);
+                            Stringdata[counter] = line;
+                        }
+                    }
+                    //for (int i = 0; i<=counter; i++)
+                    //{
+                      //  textBox1.Text=Stringdata[counter];
+                   // }
+                    
+                   
+                //}
+
+               /* catch (Exception e)
+                {
+                    // Let the user know what went wrong.
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }*/
+   
+            }
+
+        }
+
+
+        public int counter { get; set; }
+    }
+}
---
+++ twitterapp/data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace twitterapp
+{
+    class data
+    {
+        private String[] twitterNames;
+        
+        public void setLength(int length)
+        {
+            //new twitterNames[length];
+        }
+    }
+}
