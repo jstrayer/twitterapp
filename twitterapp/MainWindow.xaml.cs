ef90c9c92b56b9e66e40e076ba5ddae8b6a1e291
Justin Strayer <justincstrayer@gmail.com>
2011-10-28 21:01:30 -0500

    Removed twitter wrapper.  Early stages of the twitter names list data structure. Still music build the twitter API wrapper.  Finish the data structure.

--- twitterapp/MainWindow.xaml.cs
+++ twitterapp/MainWindow.xaml.cs
@@ -15,11 +15,13 @@ using System.IO;
 
 namespace twitterapp
 {
+     
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        dataInfo list1 = new dataInfo();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,17 +32,21 @@ namespace twitterapp
             String tweetMessage;
             tweetMessage = textBox1.Text;
 
-
+            for(int i=0;i != null ;i++)
+            {
+               textBox1.Text = list1.getValues(i);                       
+            }
         }
 
         private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
-          
+            
         }
 
         public void button2_Click(object sender, RoutedEventArgs e)
         {
-            // Configure open file dialog box
+                        
+           // Configure open file dialog box
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.FileName = "Document"; // Default file name
             dlg.DefaultExt = ".txt"; // Default file extension
@@ -61,32 +67,16 @@ namespace twitterapp
                 //{
                     // Create an instance of StreamReader to read from a file.
                     // The using statement also closes the StreamReader.
-                    using (StreamReader sr = new StreamReader(dlg.FileName))
-                    {
-                        String line;
-                        // Read and display lines from the file until the end of
-                        // the file is reached.
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            int counter = 0;
-                            Console.WriteLine(line);
-                            Stringdata[counter] = line;
-                        }
-                    }
-                    //for (int i = 0; i<=counter; i++)
-                    //{
-                      //  textBox1.Text=Stringdata[counter];
-                   // }
-                    
-                   
-                //}
-
-               /* catch (Exception e)
+                using (StreamReader sr = new StreamReader(dlg.FileName))
                 {
-                    // Let the user know what went wrong.
-                    Console.WriteLine("The file could not be read:");
-                    Console.WriteLine(e.Message);
-                }*/
+                    String line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        list1.addItem(line);
+                    }
+                }
    
             }
 
